13727acc42c43fd7b89edecbc81fa5dafbd4068e
0jihye0 <131947418+0jihye0@users.noreply.github.com>
2023-07-20 17:14:15 +0900

    3주차 django 개발 중

--- my_project/urls.py
+++ my_project/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-     path('admin/', admin.site.urls),
-     path('mysite/', include('mysite.urls')),
-     path('', include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path('mysite/', include('mysite.urls')),
+    path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
